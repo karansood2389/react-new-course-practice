2683021d53cc6f7a7354097ff2fa70e4e6d3d4e0
karansood2389 <karansood2march@gmail.com>
2021-06-10 18:41:08 +0530

    First Commit of the module

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import { useState } from "react";
-import Cart from "./Components/Cart/Cart";
-import Header from "./Components/Layout/Header/header";
-import Meals from "./Components/Meals/Meals";
-import ContextProvider from "./store/ContextProvider";
+import React from 'react';
+
+import './App.css';
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
   return (
-    <ContextProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShow={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
-    </ContextProvider>
+    <div className="app">
+      <h1>Hi there!</h1>
+    </div>
   );
 }
 
