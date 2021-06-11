17b33998c6a7948b284649f9674f425a17f41749
karansood2389 <karansood2march@gmail.com>
2021-06-11 10:25:18 +0530

    Last Commit for Module

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import React, { useCallback, useState } from "react";
+import React, { useMemo, useState } from "react";
 
 import "./App.css";
-import DemoOutput from "./components/Demo/DemoOutput";
+import DemoList from "./components/Demo/DemoList";
 import Button from "./components/UI/Button/Button";
 
 function App() {
-  const [showParaGraph, setShowParaGraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
-  const toggleButtonHandler = useCallback(() => {
-    if (allowToggle) {
-      setShowParaGraph((showParagraph) => !showParagraph);
-    }
-  }, [allowToggle]);
+  const [listsTitle, setListsTitle] = useState('My List');
 
-  const allowToggleHandler = () => {
-    setAllowToggle((allowToggle) => !allowToggle )
+  const changeTitleHandler = () => {
+    setListsTitle('New Lists')
   }
   console.log('App Running');
+  const listItems = useMemo(() => [5,1,4,3,2], []);
+
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParaGraph} />
-      <Button onClick={allowToggleHandler}>Allow Toggle!</Button>
-      <Button onClick={toggleButtonHandler}>Toggle Paragraph!</Button>
+      <DemoList title={listsTitle} items={listItems}/>
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
 
 export default App;
+// import React, { useCallback, useState } from "react";
+
+// import DemoOutput from "./components/Demo/DemoOutput";
+
+// const [showParaGraph, setShowParaGraph] = useState(false);
+// const [allowToggle, setAllowToggle] = useState(false);
+
+// const toggleButtonHandler = useCallback(() => {
+//   if (allowToggle) {
+//     setShowParaGraph((showParagraph) => !showParagraph);
+//   }
+// }, [allowToggle]);
+
+// const allowToggleHandler = () => {
+//   setAllowToggle((allowToggle) => !allowToggle )
+// }
+
+/* <DemoOutput show={showParaGraph} />
+<Button onClick={allowToggleHandler}>Allow Toggle!</Button>
+<Button onClick={toggleButtonHandler}>Toggle Paragraph!</Button> */
